f260808ab5c63a10320061df6eb731b690ed1d85
garciarodrigo99 <alu0101154473@ull.edu.es>
2022-11-08 11:59:49 +0000

    Color Text

--- p06_automata_simulator/p06_automata_simulator_original/cya-P06-Automata-Simulator.cc
+++ p06_automata_simulator/p06_automata_simulator_original/cya-P06-Automata-Simulator.cc
@@ -64,8 +64,9 @@ int main(int argc, char *argv[]) {
 
   while (getline(chainFile,chainFileLine)) {
     std::cout << chainFileLine << " --- ";
-    if (myAutomata.acceptChain(chainFileLine)) std::cout << "Accepted\n";
-    else std::cout << "Rejected\n";
+    if (myAutomata.acceptChain(chainFileLine)) 
+      std::cout << "\033[;32m Accepted\033[0m\n";
+    else std::cout << "\033[;31m Rejected\033[0m\n";
   }
 
   return 0;
